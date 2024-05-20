af3f55e50c7fcd4583612e8f16abffed8b7be142
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-20 11:04:55 -0400

    Create start/stop functionality for counter

--- js/challenge.js
+++ js/challenge.js
@@ -1,4 +1,5 @@
 const counterDisplay = document.getElementById("counter")
+const pauseBtn = document.getElementById("pause")
 let counter = 0
 let likeCounter = 0
 let counterInterval
@@ -16,22 +17,26 @@ const decreaseCounter = () => {
 }
 
 const stopCounter = () => {
-
+  if (pauseBtn.textContent === "resume"){
+    counterInterval = setInterval(incrementCounter, 1000)
+    pauseBtn.textContent = "pause"
+  } else {
+    clearInterval(counterInterval)
+    counterInterval = null
+    pauseBtn.textContent = "resume"
+  }
 }
 
 const likeMethod = () => {
   const likeSection = document.querySelector("ul.likes") 
   if (currentCounter === counter) {
     const nodeCount = likeSection.childElementCount 
-    // debugger
     likeCounter++
     likeSection.childNodes[nodeCount - 1].textContent = `${currentCounter} has been liked ${likeCounter} times`
   } else {
-    // debugger
     likeCounter = 1
     currentCounter = counter
     const li = document.createElement("li")
-    // li.id = `${counter}`
     li.textContent = `${counter} has been liked ${likeCounter} times`
     likeSection.appendChild(li)
   }
@@ -44,12 +49,15 @@ const init = () => {
   const minusBtn = document.getElementById("minus")
   const likeBtn = document.getElementById("heart")
 
-  if (!counterInterval)
-    counterInterval = setInterval(incrementCounter, 3000)
+  
+  counterInterval = setInterval(incrementCounter, 1000)
 
-plusBtn.addEventListener("click", incrementCounter)
-minusBtn.addEventListener("click", decreaseCounter)
-likeBtn.addEventListener("click", likeMethod)
+  plusBtn.addEventListener("click", incrementCounter) 
+  minusBtn.addEventListener("click", decreaseCounter)
+  likeBtn.addEventListener("click", likeMethod)
+  pauseBtn.addEventListener("click", stopCounter)
+
+
 
 
 }
