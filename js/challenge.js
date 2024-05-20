dfb803031e82783c8d60de88d3d26d833c0b3234
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-20 09:24:58 -0400

    Turn off example js and create init function

---
+++ js/challenge.js
@@ -0,0 +1,19 @@
+const counterDisplay = document.getElementById("counter")
+let counter = 0
+
+const counterMethod = () => {
+
+}
+
+const init = () => {
+
+  setInterval(counterMethod(), 1000)
+
+
+
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", init())
