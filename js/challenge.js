802d3136340219c1fb418f1c74ed2cc77d90bc99
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-20 09:55:44 -0400

    Increase like buttons counts and display it

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,6 @@
 const counterDisplay = document.getElementById("counter")
 let counter = 0
+let likeCounter = 0
 let counterInterval
 
 const incrementCounter = () => {
@@ -16,17 +17,31 @@ const stopCounter = () => {
 
 }
 
+const likeMethod = () => {
+  likeCounter++
+  const likeSection = document.querySelector("ul.likes") 
+  likeSection.textContent = `${counter} has been liked ${likeCounter} times`
+}
+
 
 const init = () => {
+  let currentCounter
+
   const plusBtn = document.getElementById("plus")
   const minusBtn = document.getElementById("minus")
+  const likeBtn = document.getElementById("heart")
 
   if (!counterInterval)
     counterInterval = setInterval(incrementCounter, 1000)
 
 plusBtn.addEventListener("click", incrementCounter)
 minusBtn.addEventListener("click", decreaseCounter)
-
+if (currentCounter !== counter){
+  currentCounter = counter
+  likeBtn.addEventListener("click", likeMethod)
+} else {
+  likeBtn.addEventListener("click", likeMethod)
+}
 
 
 
